26416ec739ef6eea36b57264d86a4ddd779bc4c3
enessancar <enes57751907@gmail.com>
2023-12-07 22:52:56 +0300

    I created auth manager , auth view model

---
+++ MusicApp/Networking/Service/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  MusicApp
+//
+//  Created by Enes Sancar on 7.12.2023.
+//
+
+import Foundation
--- MusicApp/Screens/Auth/AuthViewModel.swift
+++ MusicApp/Screens/Auth/AuthViewModel.swift
@@ -5,4 +5,52 @@
 //  Created by Enes Sancar on 1.12.2023.
 //
 
-import Foundation
+import FirebaseAuth
+
+final class AuthViewModel {
+    
+    lazy var authManager = FirebaseAuthManager()
+    
+    func login(email: String, password: String, completion: @escaping () -> Void){
+        authManager.signIn(email: email, password: password) {
+            completion()
+        } onError: { error in
+            print(error.localizedDescription)
+        }
+    }
+    
+    func register(userName: String, email: String, password: String, completion: @escaping () -> Void) {
+        authManager.register(userName: userName,
+                             email: email,
+                             password: password) {
+            completion()
+        } onError: { error in
+            print(error)
+        }
+    }
+    
+    func resetPassword(email: String, completion: @escaping (Bool, String) -> Void) {
+        guard !email.isEmpty else {
+            completion(false, "E-mail cannot be blank.")
+            return
+        }
+        
+        authManager.resetPassword(email: email) { [weak self] in
+            guard let self else { return }
+            
+            completion(true, "Please check your e-mail to reset your password.")
+            
+        } onError: { [weak self] error in
+            guard let self else { return }
+            completion(false, error)
+        }
+    }
+    
+    func signInGoogle(credential: AuthCredential, username: String, completion: @escaping () -> Void) {
+        authManager.signInWithCredential(credential: credential, username: username) {
+            completion()
+        } onError: { error in
+            print(error)
+        }
+    }
+}
