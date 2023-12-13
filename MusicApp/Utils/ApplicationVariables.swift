9278b49eda837223dd97f2c5cf68ea62e3193915
enessancar <enes57751907@gmail.com>
2023-12-13 15:24:10 +0300

    utils created

--- MusicApp/Utils/ApplicationVariables.swift
+++ MusicApp/Utils/ApplicationVariables.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct ApplicationVariables {
+    
+    private static let defaults = UserDefaults.standard
+    
+    private init() {}
+    
+    static var currentUserID: String?{
+        get{
+            return defaults.string(forKey: "currentUserID")
+        } set {
+            defaults.set(newValue, forKey: "currentUserID")
+        }
+    }
+    
+    static func resetApplicationVariables(){
+        defaults.removeObject(forKey: "currentUserID")
+    }
+    
+}
--- MusicApp/Utils/WorkItem.swift
+++ MusicApp/Utils/WorkItem.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+/// To be triggered after the user stops typing 
+
+final class WorkItem {
+    
+    private var pendingRequestWorkItem: DispatchWorkItem?
+    
+    func perform(after: TimeInterval, _ block: @escaping () -> Void) {
+        // Cancel the currently pending item
+        pendingRequestWorkItem?.cancel()
+        
+        // Wrap our request in a work item
+        let requestWorkItem = DispatchWorkItem(block: block)
+        
+        pendingRequestWorkItem = requestWorkItem
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + after, execute: requestWorkItem)
+    }
+}
